f2986a4417f95968a1165837efefa64cebb10e5a
robotronic12 <fran120304@gmail.com>
2023-05-09 14:37:30 +0200

    Barra Mejorada

--- Script.js
+++ Script.js
@@ -1,12 +1,23 @@
 //Archivo de Java
 let ublicacionPrincipal= window.pageYOffset; //toma la ubicacion de la y
 window.onscroll= function(){
-	var Barra=document.getElementById("barraDeNavegacion")
+	/*var Barra=document.getElementById("barraDeNavegacion")
 	let Desplazamiento_Actual= window.pageYOffset;
 	if (ublicacionPrincipal >= Desplazamiento_Actual){
 		Barra.className= 'barraDeNavegacion';
 	}else{
 		Barra.className= 'barraDeNavegacionMinimizada';
 	}
-	ublicacionPrincipal= Desplazamiento_Actual;
+	ublicacionPrincipal= Desplazamiento_Actual;*/
+
+
+	var Barra=document.getElementById("barraDeNavegacion");
+	var tope = 90;
+	let Desplazamiento_Actual= window.pageYOffset;
+
+	if (Desplazamiento_Actual>tope) {
+		Desplazamiento_Actual = tope;
+	}
+	let a = "-" + Desplazamiento_Actual + "px"
+	Barra.style.top = a;
 }
